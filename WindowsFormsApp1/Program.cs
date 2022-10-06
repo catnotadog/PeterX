1ddd28346098b944e10ffebb853764a642123569
catnotadog <17masonman9@gmail.com>
2022-10-06 18:49:53 -0400

    Add project files.

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using sxlib;
+using sxlib.Specialized;
+
+namespace PeterX
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] Args)
+        {
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+        public static void peteralerttrigger()
+        {
+            Application.Run(new Form2());
+        }
+        public static void peteralertstrigger()
+        {
+            Application.Run(new Form2(true));
+        }
+
+    }
+}
